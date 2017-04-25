3f3fe180b786e9bcc0b1c5fd4ca0c38fb8e9e577
shelley <616600490@qq.com>
2017-04-25 08:38:27 +0800

    7.4 v-show

--- src/pages/example.vue
+++ src/pages/example.vue
@@ -1,10 +1,10 @@
 <template>
 	<div class="home">
 		<h1>Hello, Vue.js!</h1>
-		<h1 v-if="yes">Yes!</h1>
-		<h1 v-if="no">No!</h1>
-		<h1 v-if="age >= 25">Age: {{ age }}</h1>
-		<h1 v-if="name.indexOf('jack') >= 0">Name: {{ name }}</h1>
+		<h1 v-show="yes">Yes!</h1>
+		<h1 v-show="no">No!</h1>
+		<h1 v-show="age >= 25">Age: {{ age }}</h1>
+		<h1 v-show="name.indexOf('keep') >= 0">Name: {{ name }}</h1>
 	</div>
 </template>
 <style>
@@ -18,7 +18,7 @@
 			return {
 				yes: true,
 				no: false,
-				age: 28,
+				age: 24,
 				name: 'keepfool'
 			};
 		},
